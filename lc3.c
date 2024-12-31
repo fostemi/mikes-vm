9877b11be34effa3dc7dd506f68139abecbecf65
fostemi <michael.isaac.foster@gmail.com>
2024-12-30 21:08:17 -0600

    mac specific functionality

--- lc3.c
+++ lc3.c
@@ -49,6 +49,47 @@ enum {
   OP_TRAP
 };
 
+enum {
+  TRAP_GETC = 0x20,
+  TRAP_OUT = 0x21,
+  TRAP_PUTS = 0x22,
+  TRAP_IN = 0x23,
+  TRAP_PUTSP = 0x24,
+  TRAP_HALT = 0x25
+}
+
+enum {
+  MR_KBSR = 0xFE00,
+  MR_KBDR = 0xFE02
+}
+
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
 int main(argc int, const char* argv[]) {
   if (argc < 2) {
     printf("lc3 [image-file1 ...\n]");
@@ -152,7 +193,43 @@ int main(argc int, const char* argv[]) {
         reg[r0] = reg[R_PC] + pc_offset;
         update_flags(r0);
       case OP_TRAP:
-        break;
+        reg[R_R7] = reg[R_PC];
+        switch(instr & 0xFF) {
+          case TRAP_GETC:
+            reg[R_R0] = (uint16_t)getchar();
+            update_flags(R_R0);
+          case TRAP_OUT:
+            putc((char)reg[R_R0], stdout);
+            fflush(stdout);
+          case TRAP_PUTS:
+            uint16_t* c = memory + reg[R_R0];
+            while(*c) {
+              putc((char)*c, stdout);
+              ++c;
+            }
+            fflush(stdout);
+          case TRAP_IN:
+            printf("Enter a character: ");
+            char c = getchar();
+            putc(c, stdout);
+            fflush(stdout);
+            reg[R_R0] = (uint16_t)c;
+            update_flags(R_R0);
+          case TRAP_PUTSP:
+            uint16_t* c = memory + reg[R_R0];
+            while(*c) {
+              char char1 = (*c) 0xFF;
+              putc(char1, stdout);
+              char char2 = (*c) >> 8;
+              if (char2) putc(char2, stdout);
+              ++c;
+            }
+            fflush(stdout);
+          case TRAP_HALT:
+            puts("HALT");
+            fflush(stdout);
+            running = 0;
+        }
       case OP_RES:
         abort();
       case OP_RTI:
@@ -182,4 +259,48 @@ void update_flags(uint16_t r){
   else {
     reg[R_COND] = FL_POS;
   }
+}
+
+void read_image_file(FILE* file){
+  uint16_t origin;
+  free(&origin, sizeof(origin), 1, file);
+  origin = swap16(origin);
+
+  uint16_t max_read = MEMORY_MAX - origin;
+  uint16_t* p = memory + origin;
+  size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+  while(read-- > 0){
+    *p = swap16(*p);
+    ++p;
+  }
+}
+
+uint16_t swap16(uint16_t x){
+  return (x << 8) | (x >> 8);
+}
+
+void read_image(const char* image_path){
+  FILE* file = fopen(image_path, "rb");
+  if (!file) { return 0; };
+  read_image_file(file);
+  fclose(file);
+  return 1;
+}
+
+void mem_write(uint16_t address, uint16_t val){
+  memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address){
+  if(address == MR_KBSR){
+    if(check_key()){
+      memory[MR_KBSR] = (1 << 15);
+      memory[MR_KBDR] = getchar();
+    }
+    else {
+      memory[MR_KBSR] = 0;
+    }
+  }
+  return memory[address];
 }
